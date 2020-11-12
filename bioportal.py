c53e3827acb9bde85c3ff9689e27cfa15b8662ea
Marco Brandizi <marco.brandizi@gmail.com>
2020-11-12 22:14:48 +0000

    Adding descriptor retrieval.

--- bioportal.py
+++ bioportal.py
@@ -1,3 +1,6 @@
+"""
+  Hacky implementation of clients to invoke Bioportal APIs.
+"""
 import platform
 majorv = platform.python_version()
 majorv = int ( majorv [ 0 ] )
@@ -11,59 +14,114 @@ else:
 import json
 import sys
 
-
 BIOPORTAL_BASE_URL = "http://data.bioontology.org"
 
+is_debug = False
+
+"""
+  Builds a URL parameter, including the URL-encoding of its value.
+"""
 def url_param ( key, value ):
 	return key + "=" + quote ( value )
 
+"""
+  The same as url_param, but prepend a '&' separator.
+"""
 def url_param_append ( key, value ):
 	return "&" + url_param ( key, value )
 
+"""
+  Builds a URL from a dictionary of parameters.
+"""
 def url_build ( baseurl, **params ):
 	url = baseurl
 	for key in params:
 		url += url_param_append ( key, params [ key ] )
 	return url
 
+"""
+	Simple helper to get the URL content
+"""
+def url_get_json ( url ):
+	if is_debug: print ( "url_get_json: " + url )
+	jdoc = urlopen ( url ).read()
+	jdoc = json.loads( jdoc )
+	return jdoc
+
+
+"""
+	The BioPortal client.
+
+	Instances must be inititalised with an API key, which is then reused as necessary.
+"""
 class BioPortalClient:
 	def __init__ ( self, apikey ):
 		self.apikey = apikey
 
+	# The API base URL, including the apikey
 	def _bp_baseurl ( self, api_path ):
 		return BIOPORTAL_BASE_URL + api_path + "?" + url_param ( "apikey", self.apikey )
 
+	"""
+		The text annotation service.
+
+		This returns the plain json
+	"""
 	def annotator ( self, text, **other_params ):
 		other_params [ "text" ] = text
 		url = self._bp_baseurl ( "/annotator" )
 		url = url_build ( url, **other_params )
-		# DEBUG print ( url )
-		jterms = urlopen ( url ).read()
-		jterms = json.loads( jterms )
+		jterms = url_get_json ( url )
 		return jterms
 
-	def annotator_terms ( self, text, cutoff = -1, **other_params ):
+	"""
+		The text annotation service.
+
+		Returns the a list term descriptors, which are also cutoff based on the corresponding parameter.
+		The annotar should return terms in order of significance, so the cutoff should retain the most relevant ones.
+
+		if get_uris is set, returns only the term URIs (instead of complete dictionaries with label, synonym, definition).
+	"""
+	def annotator_terms ( self, text, cutoff = None, only_uris = False, **other_params ):
 		jterms = self.annotator ( text, **other_params )
-		terms = [ term [ "annotatedClass" ] [ "@id" ] for term in jterms ]
-		# Strangely, there are dupes
+		# Strangely, there are dupes, let's filter
 		visited = set ()
 		new_terms = []
-		for term in terms:
-			if term in visited: continue
-			new_terms.append ( term )
-			visited.add ( term )
-		if cutoff != -1 and len ( new_terms ) > cutoff:
-			new_terms = new_terms [ 0: cutoff ] 
+		for term in jterms:
+			uri = term [ "annotatedClass" ] [ "@id" ]
+			if uri in visited: continue
+			new_term = { "uri": term [ "annotatedClass" ] [ "@id" ] }
+			new_term [ "self" ] = term [ "annotatedClass" ] [ "links" ] [ "self" ]
+			new_terms.append ( new_term )
+			visited.add ( uri )
+		if cutoff and len ( new_terms ) > cutoff:
+			new_terms = new_terms [ 0: cutoff ]
+		if only_uris:
+			new_terms = [ term [ "uri" ] for term in new_terms ]
+			return new_terms
+		# Else, let's resolve only those that were retained
+		for new_term in new_terms:
+			durl = new_term [ "self" ]
+			durl += "?" + url_param ( "apikey", self.apikey )
+			dj = url_get_json ( durl )
+			new_term [ "label" ] = dj [ "prefLabel" ]
+			new_term [ "synonyms" ] = dj [ "synonym" ]
+			defv = dj [ "definition" ]
+			defv = defv [ 0 ] if len ( defv ) > 0 else "" # TODO: why is it string?
+			new_term [ "definition" ] = defv
 		return new_terms
+		
 
-if __name__ == '__main__DISABLED':
+
+if __name__ == '__main__':
 	# TODO: this is what their UI uses: 8b5b7825-538d-40e0-9e9e-5ab9274a9aeb
 	if len ( sys.argv ) < 2:
 	  #print ( "A Bioportal API key is needed to test this module (as command line argument). Skipping tests" )
 		pass
 	else:
+		is_debug = True
 		bp = BioPortalClient ( sys.argv [ 1 ] )
 		#terms = bp.annotator_terms ( "Melanoma is a malignant tumor usually affecting the skin", cutoff = 3, ontologies = "MESH,SNOMED,ICD10" )
-		terms = bp.annotator_terms ( "Melanoma is a malignant tumor usually affecting the skin", 3 )
+		terms = bp.annotator_terms ( "Melanoma is a malignant tumor usually affecting the skin", 5 )
 		terms = terms [ 0: 9 ]
 		print ( terms )
